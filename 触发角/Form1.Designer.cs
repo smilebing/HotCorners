1b14744fc90fc6e4f078ea0477ccb694c08b5823
pc <zhudahe@outlook.com>
2017-12-20 13:31:50 +0800

    pc
    增加左上角和有下角

---
+++ 触发角/Form1.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace 触发角
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.显示ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.退出ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(47, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(49, 53);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 12);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "label2";
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(49, 87);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(100, 96);
+            this.richTextBox1.TabIndex = 2;
+            this.richTextBox1.Text = "";
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.ContextMenuStrip = this.contextMenuStrip1;
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "notifyIcon1";
+            this.notifyIcon1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon1_MouseDoubleClick);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.显示ToolStripMenuItem,
+            this.退出ToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(101, 48);
+            // 
+            // 显示ToolStripMenuItem
+            // 
+            this.显示ToolStripMenuItem.Name = "显示ToolStripMenuItem";
+            this.显示ToolStripMenuItem.Size = new System.Drawing.Size(100, 22);
+            this.显示ToolStripMenuItem.Text = "显示";
+            // 
+            // 退出ToolStripMenuItem
+            // 
+            this.退出ToolStripMenuItem.Name = "退出ToolStripMenuItem";
+            this.退出ToolStripMenuItem.Size = new System.Drawing.Size(100, 22);
+            this.退出ToolStripMenuItem.Text = "退出";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.SizeChanged += new System.EventHandler(this.Form1_SizeChanged);
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem 显示ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 退出ToolStripMenuItem;
+    }
+}
+
---
+++ 触发角/Form1.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+
+namespace 触发角
+{
+    public partial class Form1 : Form
+    {
+        private int Width;
+        private int Height;
+        private bool canShowLeftTop = true;
+        private bool canShowRightBottom = true;
+        private ContextMenu notifyiconMnu;
+
+        public Form1()
+        {
+            InitializeComponent();
+            //定义一个MenuItem数组，并把此数组同时赋值给ContextMenu对象 
+            MenuItem[] mnuItms = new MenuItem[3];
+            mnuItms[0] = new MenuItem();
+            mnuItms[0].Text = "显示窗口";
+            mnuItms[0].Click += new System.EventHandler(this.notifyIcon1_showfrom);
+
+            mnuItms[1] = new MenuItem("-");
+
+            mnuItms[2] = new MenuItem();
+            mnuItms[2].Text = "退出系统";
+            mnuItms[2].Click += new System.EventHandler(this.ExitSelect);
+            mnuItms[2].DefaultItem = true;
+
+            notifyiconMnu = new ContextMenu(mnuItms);
+            notifyIcon1.ContextMenu = notifyiconMnu;
+            //为托盘程序加入设定好的ContextMenu对象 
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //  2.当前的屏幕包括任务栏的工作域大小
+            Width = Screen.PrimaryScreen.Bounds.Width;
+            Height = Screen.PrimaryScreen.Bounds.Height;
+            richTextBox1.Text += Width;
+            richTextBox1.Text += Height;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            var p = Cursor.Position;
+            label1.Text = p.X.ToString();
+            label2.Text = p.Y.ToString();
+
+            doShow(p.X, p.Y);
+        }
+
+        void doShow(int x, int y)
+        {
+
+            if (y < 20 && x < 20)
+            {
+                if (canShowLeftTop)
+                {
+                    canShowLeftTop = false;
+                    showWinTab();
+                }
+            }
+            else if (y > Height - 10 && x > Width - 10)
+            {
+                if (canShowRightBottom)
+                {
+                    canShowRightBottom = false;
+                    showWinD();
+                }
+            }
+            else
+            {
+                canShowRightBottom = true;
+                canShowLeftTop = true;
+            }
+        }
+
+
+
+
+
+        #region 发送按键
+
+        [DllImport("user32.dll", EntryPoint = "keybd_event", SetLastError = true)]
+        private static extern void keybd_event(Keys bVk, byte bScan, uint dwFlags, uint dwExtraInfo);
+
+
+        private void showWinTab()
+        {
+            //触发操作
+            //符号键模拟 Control+Shift+Z 调取搜狗输入法符号页面
+            keybd_event(Keys.LWin, 0, 0, 0);
+            keybd_event(Keys.Tab, 0, 0, 0);
+
+            keybd_event(Keys.LWin, 0, 0x02, 0);
+            keybd_event(Keys.Tab, 0, 0x02, 0);
+        }
+
+        private void showWinD()
+        {
+            //触发操作
+            //符号键模拟 Control+Shift+Z 调取搜狗输入法符号页面
+            keybd_event(Keys.LWin, 0, 0, 0);
+            keybd_event(Keys.D, 0, 0, 0);
+
+            keybd_event(Keys.LWin, 0, 0x02, 0);
+            keybd_event(Keys.D, 0, 0x02, 0);
+        }
+        #endregion
+
+        private void Form1_SizeChanged(object sender, EventArgs e)
+        {
+            //判断是否选择的是最小化按钮
+            if (WindowState == FormWindowState.Minimized)
+            {
+                //隐藏任务栏区图标
+                this.ShowInTaskbar = false;
+                //图标显示在托盘区
+                notifyIcon1.Visible = true;
+            }
+        }
+
+        public void notifyIcon1_showfrom(object sender, System.EventArgs e)
+        {
+            this.Show();
+            this.ShowInTaskbar = true;
+            this.WindowState = FormWindowState.Normal;
+            notifyIcon1.Visible = false;
+
+        }
+
+        public void ExitSelect(object sender, System.EventArgs e)
+        {
+            //隐藏托盘程序中的图标 
+            notifyIcon1.Visible = false;
+            //关闭系统 
+            Close();
+            Dispose(true);
+        }
+
+        private void notifyIcon1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            if (WindowState == FormWindowState.Minimized)
+            {
+                //还原窗体显示    
+                WindowState = FormWindowState.Normal;
+                //激活窗体并给予它焦点
+                this.Activate();
+                //任务栏区显示图标
+                this.ShowInTaskbar = true;
+                //托盘区图标隐藏
+                notifyIcon1.Visible = false;
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            notifyIcon1.Visible = true;
+            notifyIcon1.ShowBalloonTip(500, "提示", "触发角程序后台运行.", ToolTipIcon.Info);
+            Hide();
+            e.Cancel = true;
+        }
+    }
+}
